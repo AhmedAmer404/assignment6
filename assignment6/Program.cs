a014d32bedf2daefd3aed44650aa775549f71215
Ahmed Amer <ahmedamerxx5@gmail.com>
2026-09-15 17:39:15 +0200

    question 3

--- assignment6/Program.cs
+++ assignment6/Program.cs
@@ -8,6 +8,9 @@ namespace assignment6
         private string password = "secret";
         internal int copiesInStock = 5;
 
+        public string Title;
+
+
     
     }
 
@@ -24,6 +27,12 @@ namespace assignment6
             #region Question 2
             Console.WriteLine(book.copiesInStock);
             // This will print 5 internal member and can be accessed within the same Project
+            #endregion
+
+            #region Question 3
+            book .Title = "C# Programming";
+            Console.WriteLine(book.Title);
+            // This will print "C# Programming" public member can be accessed from anywhere
             #endregion
 
         }
