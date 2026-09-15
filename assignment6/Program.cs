fd02690f7257f363850d9b5f7fc9926d589d8bb6
Ahmed Amer <ahmedamerxx5@gmail.com>
2026-09-15 17:34:57 +0200

    question 1

--- assignment6/Program.cs
+++ assignment6/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+
+namespace assignment6
+{
+    class Book
+    {
+        private string password = "secret";
+    
+    }
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            #region Question 1
+            Console.WriteLine("Book object created.");
+            // Error private access modifiers can be accessed only within the class
+            #endregion
+
+        }
+    }
+}
