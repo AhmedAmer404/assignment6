1063b63284e3fc12e13e2ca2b79a60c1648df780
Ahmed Amer <ahmedamerxx5@gmail.com>
2026-09-15 17:37:36 +0200

    question 2

--- assignment6/Program.cs
+++ assignment6/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Reflection;
 
 namespace assignment6
 {
     class Book
     {
         private string password = "secret";
+        internal int copiesInStock = 5;
+
     
     }
 
@@ -16,6 +19,11 @@ namespace assignment6
             #region Question 1
             Console.WriteLine("Book object created.");
             // Error private access modifiers can be accessed only within the class
+            #endregion
+
+            #region Question 2
+            Console.WriteLine(book.copiesInStock);
+            // This will print 5 internal member and can be accessed within the same Project
             #endregion
 
         }
